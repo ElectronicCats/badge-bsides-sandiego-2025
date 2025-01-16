093b4c351a14149edd1989e2ec4cb55388f9af61
deimos <deimoshall@gmail.com>
2025-01-16 12:52:58 -0600

    feat: save reboot counter on flash

--- firmware/bsides_badge/User/main.c
+++ firmware/bsides_badge/User/main.c
@@ -21,8 +21,26 @@
  *
  */
 
+#include <limits.h>
 #include "bitmaps.h"
+#include "ch32v003_flash.h"
 #include "debug.h"
+
+// Define this constant to use compile-time
+// flash addresses in the below example code.
+#define USE_COMPILE_TIME_ADDRESSES 1
+
+#ifdef USE_COMPILE_TIME_ADDRESSES
+  // Compile-time determined address for flash memory
+  #define NONVOLATILE_START_ADDR FLASH_PRECALCULATE_NONVOLATILE_ADDR(0)
+  #define NONVLOATILE_VAR_ADDR   FLASH_PRECALCULATE_NONVOLATILE_ADDR(10)
+#else
+// Runtime determined address for flash memory
+uint32_t nonvolatile_var_addr;
+uint32_t nonvolatile_start_addr;
+#endif
+
+uint16_t valueInFlash;
 
 /* Global define */
 
@@ -187,6 +205,88 @@ void save_HIGHSCORE_TTRIS(void);
 void Check_NEW_RECORD(void);
 uint8_t checksum(uint8_t Byte_);
 
+void flash_test() {
+  // Set the flash latency for proper operation.
+  flash_set_latency();
+  APP_DBG("FLASH_LENGTH_OVERRIDE is        %u\r\n",
+          (uint16_t) (uintptr_t) FLASH_LENGTH_OVERRIDE);
+
+#ifdef USE_COMPILE_TIME_ADDRESSES
+  APP_DBG("non-volatile start address is   %lu\r\n", NONVOLATILE_START_ADDR);
+  APP_DBG("non-volatile var address is     %lu\r\n", NONVLOATILE_VAR_ADDR);
+#else
+  nonvolatile_start_addr = flash_calcualte_nonvolatile_addr(
+      0);  // Calculate the start address for non-volatile data at runtime.
+  nonvolatile_var_addr = flash_calcualte_nonvolatile_addr(
+      10);  // Calculate the variable address for non-volatile data at runtime.
+  APP_DBG("non-volatile start address is   %lu\r\n", nonvolatile_start_addr);
+  APP_DBG("non-volatile var address is     %lu\r\n", nonvolatile_var_addr);
+#endif
+
+  int writeCount = 0;
+
+#ifdef USE_COMPILE_TIME_ADDRESSES
+  valueInFlash = flash_read_16_bits(
+      NONVLOATILE_VAR_ADDR);  // Read a 16-bit value from flash memory at the
+                              // predefined address.
+  if (valueInFlash == UINT16_MAX) {
+    APP_DBG("Value not found. Setting to 0.\r\n");
+    valueInFlash = 0;
+  }
+#else
+  valueInFlash = flash_read_16_bits(
+      nonvolatile_var_addr);  // Read a 16-bit value from flash memory at the
+                              // runtime determined address.
+#endif
+
+  APP_DBG("   Saved value is %u\r\n", valueInFlash);
+
+  // Prevent more than 5 writes per power-on, as writing to flash is a limited
+  // resource.
+  if (writeCount >= 5) {
+    APP_DBG("Done writing. Wait...\r\n");
+    Delay_Ms(1000);  // Delay for 1000 milliseconds (1 second).
+    // continue;
+  } else {
+    writeCount++;
+
+    // Decrease the value to be written to flash by 1
+    valueInFlash++;
+
+    // Unlock writing to flash memory
+    flash_unlock();
+    APP_DBG("Memory unlocked\r\n");
+
+    // This erases a 64-byte page in the flash memory.
+    // Note: smaller erases are not possible on the CH32V003.
+#ifdef USE_COMPILE_TIME_ADDRESSES
+    flash_erase_page(
+        NONVOLATILE_START_ADDR);  // Erase a 64-bit block of flash memory at the
+                                  // predefined start address.
+#else
+    flash_erase_page(
+        nonvolatile_start_addr);  // Erase a 64-bit block of flash memory at the
+                                  // runtime determined start address.
+#endif
+    APP_DBG("Memory erased\r\n");
+
+#ifdef USE_COMPILE_TIME_ADDRESSES
+    flash_program_16(NONVLOATILE_VAR_ADDR,
+                     valueInFlash);  // Write a 16-bit value to flash memory at
+                                     // the predefined address.
+#else
+    flash_program_16(nonvolatile_var_addr,
+                     valueInFlash);  // Write a 16-bit value to flash memory at
+                                     // the runtime determined address.
+#endif
+    APP_DBG("Memory written\r\n");
+
+    // Lock flash memory
+    flash_lock();
+    APP_DBG("Memory locked. \r\nWaiting...\r\n");
+  }
+}
+
 // ===================================================================================
 // Main Function
 // ===================================================================================
@@ -219,6 +319,7 @@ int main(void) {
     JOY_DLY_ms(delay);
   }
   Delay_Ms(500);  // Keep the logo on the screen for a while
+  flash_test();
 
   // Loop
   APP_DBG("Starting...\r\n");
