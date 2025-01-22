0c0fd0fee9ad002594295ae5d5aeee571bec016f
deimos <deimoshall@gmail.com>
2025-01-22 12:15:14 -0600

    feat: add credits text

--- firmware/bsides_badge/User/menus.c
+++ firmware/bsides_badge/User/menus.c
@@ -11,14 +11,13 @@ typedef enum {
   MENU_USER_NAME,
   MENU_ABOUT,
   /* Settings layer */
-  MENU_SETTINGS,
+  // MENU_SETTINGS,
   // MENU_LEDS,
   /************/
   MENU_COUNT
 } menu_item_t;
 
-// char* menu_names[] = {"Game", "Name", "About"};
-char* menu_names[] = {"Game", "Name"};
+char* menu_names[] = {"game", "name", "about"};
 char* user_name = "Player";
 uint8_t max_user_name_length = 5;
 
@@ -29,11 +28,11 @@ static menu_item_t current_menu = MENU_GAME;
 
 void display_menu() {
   ssd1306_clear();
-  ssd1306_drawImage(menus_bitmaps[current_menu], 0, 0, 128, 32, COLOR_NORMAL);
-  // ssd1306_drawstr_sz(menu_names[current_menu], 0, 0,
+  // ssd1306_drawImage(menus_bitmaps[current_menu], 0, 0, 128, 32,
+  // COLOR_NORMAL); ssd1306_drawstr_sz(menu_names[current_menu], 0, 0,
   // COLOR_NORMAL,
   //                    fontsize_16x16);
-  // ssd1306_drawstr(menu_names[current_menu], 0, 0, COLOR_NORMAL);
+  ssd1306_drawstr(menu_names[current_menu], 0, 0, COLOR_NORMAL);
   ssd1306_refresh();
 }
 
@@ -70,11 +69,24 @@ void insert_name() {
   // }
 }
 
+/**
+ * @brief Display the credits
+ *
+ * @note Since font was modified, it only displays lowercase characters,
+ * so the spaces between words have to be calculated manually.
+ *
+ * @return void
+ */
 void display_credits() {
   ssd1306_clear();
-  ssd1306_drawstr("ab", 0, 0, COLOR_NORMAL);
-  // ssd1306_drawstr("BSides San Diego", 0, 0, COLOR_NORMAL);
-  printf("size of fontdata: %d\r\n", sizeof(fontdata));
+  ssd1306_drawstr("developed", 0, 0, COLOR_NORMAL);
+  ssd1306_drawstr("with", 10 * 8, 0, COLOR_NORMAL);
+  ssd1306_drawstr("love", 0, 8, COLOR_NORMAL);
+  ssd1306_drawstr("from", 5 * 8, 8, COLOR_NORMAL);
+  ssd1306_drawstr("mexico", 10 * 8, 8, COLOR_NORMAL);
+  ssd1306_drawstr("by", 0, 16, COLOR_NORMAL);
+  ssd1306_drawstr("electronic", 3 * 8, 16, COLOR_NORMAL);
+  ssd1306_drawstr("cats", 0, 24, COLOR_NORMAL);
   ssd1306_refresh();
 }
 
@@ -92,17 +104,17 @@ void menus_init() {
         case MENU_GAME:
           tetris_start();
           break;
-        // case MENU_SETTINGS:
-        //   break;
-        case MENU_ABOUT:
-          display_credits();
-          Delay_Ms(2000);
-          break;
         case MENU_USER_NAME:
           // insert_name();
           break;
-        // case MENU_LEDS:
-        //   break;
+        case MENU_ABOUT:
+          display_credits();
+          while (1) {
+            if (joy_left_pressed()) {
+              break;
+            }
+          }
+          break;
         default:
           break;
       }
